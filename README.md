347094a6383609370422b6473375df59d0a4e009
21600212 <21600212@handong.edu>
2021-01-18 20:11:45 +0900

    second commit

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Flask_Study
 
-## 이 레파지토리는 Flask를 이용해서 Web 서버를 만들어 보는 Tutorial를 연습해보기 위해 만들었다.
+## Flask Web Server Tutorial
 - [How to stucture a Flask-RESTPlus web service for production builds](https://www.freecodecamp.org/news/structuring-a-flask-restplus-web-service-for-production-builds-c2ec676de563/#database-models-and-migration)
 - [한글자료](https://dejavuqa.tistory.com/273)
-## 모든 라이브러리는 conda 가상환경에서 진행됐다.
+## 모든 라이브러리는 conda 가상환경에서 설치했다.
 
 ## 수행하게 될 과제 내용:
 
--- manage.py
+++ manage.py
@@ -4,10 +4,12 @@ import unittest
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 
+from app import blueprint
 from app.main import create_app, db
-from app.main.model import user
+from app.main.model import user, blacklist
 
 app = create_app(os.getenv('BOILERPLATE_ENV') or 'dev')
+app.register_blueprint(blueprint)
 
 app.app_context().push()
 
@@ -19,16 +21,16 @@ manager.add_command('db', MigrateCommand)
 
 @manager.command
 def run():
-    app.run()
+        app.run(host='0.0.0.0', port=80, threaded=True)
 
 @manager.command
 def test():
-    """Runs the unit tests."""
-    tests = unittest.TestLoader().discover('app/test', pattern='test*.py')
-    result = unittest.TextTestRunner(verbosity=2).run(tests)
-    if result.wasSuccessful():
-        return 0
-    return 1
+        """Runs the unit tests."""
+        tests = unittest.TestLoader().discover('app/test', pattern='test*.py')
+        result = unittest.TextTestRunner(verbosity=2).run(tests)
+        if result.wasSuccessful():
+                return 0
+        return 1
 
 if __name__ == '__main__':
-    manager.run()
+        manager.run()
